9319a0640b1b17209695b029f9f0a729db58c1b0
Leonides Lemus <xxluckyleoxx1@gmail.com>
2021-07-28 07:09:42 -0400

    added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def index
+        users = User.all 
+        render json: users
+    end
+
+    def login
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: user
+        else
+            render json: {error: "user does not exist or password does not match."}
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
+    
+end
